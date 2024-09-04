987a443ddf9645e26bffba1e08cb080a2cfdf70b
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-09-05 00:55:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function showAlert() {
+  
+    const firstName = document.getElementById('FirstName').value;
+    const lastName = document.getElementById('LastName').value;
+    const phoneNumber = document.getElementById('PhoneNumber').value;
+    const email = document.getElementById('EmailID').value;
+
+  
+    const alertMessage = `First Name: ${firstName}\nLast Name: ${lastName}\nPhone Number: ${phoneNumber}\nEmail ID: ${email}`;
+
+   
+    alert(alertMessage);
+}
